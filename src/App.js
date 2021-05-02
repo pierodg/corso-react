976be3b3127e12a1cac6784f1f77e0a968b13992
Piero <dg_piero@hotmail.it>
2021-05-03 01:10:20 +0200

    uso di props in un componente figlio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import FiglioStock from './FiglioStock';
+
 
 function App() {
   return (
@@ -9,6 +11,10 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+      <FiglioStock nome="APPLE" fondatore="Jobs" />
+      <FiglioStock nome="GOOGLE" fondatore="Gates" />
+      <FiglioStock />
+
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/FiglioStock.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from "prop-types";
+
+//componente con la notazione delle funzioni
+//Functional  Stateless Component
+
+const FiglioStock = (props) => {
+
+   
+     
+    return (
+        <div>
+            <h1>Io sono il Figlio {props.nome}</h1>
+            <p>Fondatore: {props.fondatore} </p>
+        </div>
+    );
+}
+
+export default FiglioStock;
+
+FiglioStock.defaultProps = {
+    nome: 'ND',
+    fondatore: 'ND'
+}
+
+FiglioStock.propTypes = {
+    nome: PropTypes.string,
+    fomndatore: PropTypes.string
+}
+
+//Types
+//string, array, bool, func, number, object, func.isRequired -> check se il campo è obbligatorio
+
